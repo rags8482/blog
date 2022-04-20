6bbeb5abb1a6b0b4a48c94ee859d77870aa073e9
Raghavendra Chowdappa <rags8482@gmail.com>
2022-04-20 23:37:24 +0530

    Add post CrUD

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,6 @@
-from django.views.generic import ListView, DetailView
+from dataclasses import fields
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
 from .models import Post
 
@@ -11,3 +13,19 @@ class BlogListView(ListView):
 class BlogDetailView(DetailView):
     model = Post
     template_name = 'post_detail.html'
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = 'post_new.html'
+    # fields = '__all__'
+    fields = ['title','author','body']
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = 'post_edit.html'
+    fields = ['title','body']
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')
